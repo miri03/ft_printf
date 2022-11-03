580e44a992ccc7be156f61ba2d8f44bc4c1d62c2
Meryem Harit <haritmeryem@gmail.com>
2022-11-03 03:24:55 +0100

    %s %d

--- libft/ft_putstr_fd.c
+++ libft/ft_putstr_fd.c
@@ -6,7 +6,7 @@
 /*   By: meharit <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/18 20:36:35 by meharit           #+#    #+#             */
-/*   Updated: 2022/11/02 14:26:26 by meharit          ###   ########.fr       */
+/*   Updated: 2022/11/03 03:23:54 by meharit          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,7 @@
 
 void	ft_putstr_fd(char *s, int fd, int *count)
 {
-	if (s != NULL && fd >= 0)
-	{
-		printf("len =%zu\n",ft_strlen(s));
-		write(fd, "12345", ft_strlen(s));
-	}
+	if (s != NULL)
+		write(fd, s, ft_strlen(s));
 	*count = *count + ft_strlen(s);
 }
--- printf.c
+++ printf.c
@@ -6,11 +6,12 @@
 /*   By: meharit <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/01 21:57:41 by meharit           #+#    #+#             */
-/*   Updated: 2022/11/02 14:26:56 by meharit          ###   ########.fr       */
+/*   Updated: 2022/11/03 01:26:32 by meharit          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include"printf.h"
+
 int	ft_printf(const char *str, ...)
 {
 	int count;
@@ -36,37 +37,35 @@ int	ft_printf(const char *str, ...)
 				ft_putchar_fd(str[i], 1);
 				count++;
 			}
-			else
+			else if (str[i] == 'd')
 			{
-				if (str[i] == 'd')
-				{
-					ft_putnbr_fd(va_arg(ptr, int), 1, &count);
-					i++; //fucked up line
-					count++; // the new fucked up line
-				}
-				if (str[i] == 's')
-				{
-				//	printf("first\n");
-					ft_putstr_fd(va_arg(ptr, char *), 1, &count);
-					i++;
-					printf("in =%d",count);
-					//count++; // the new fucked up line
-				}
-			// re	printf("\nafterr:");
-				ft_putchar_fd(str[i], 1);
-				count++;
+				ft_putnbr_fd(va_arg(ptr, int), 1, &count);
+				i++; //fucked up line
+				count++; // the new fucked up line
 			}
+			else if (str[i] == 's')
+			{
+				ft_putstr_fd(va_arg(ptr, char *), 1, &count);
+				i++;
+				//count++; // the new fucked up line
+			}
+			ft_putchar_fd(str[i], 1);
+		}
+
+		if (str[i])
+		{
+			count++;
 			i++;
 		}
 	}
-	va_end(ptr);
-	return (count);
+		va_end(ptr);
+		return (count);
 }
 
 #include<stdio.h>
 int main()
 {
 	int a = 443644;
-	int i = ft_printf("%s","0xA");
+	int i = ft_printf("ghy%shvh%sHH","0xA", "12");
 	printf("\n%d",i);
 }
